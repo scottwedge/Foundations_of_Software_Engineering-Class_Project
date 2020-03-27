96c0b689524a46b1b354052cc2dbe60176771f13
yafty <yafty@atlasdatasecurity.com>
2020-03-27 15:22:07 -0400

    fleshed out data field of message class

--- game/messages/messages.py
+++ game/messages/messages.py
@@ -2,15 +2,14 @@
 
 import json
 
-FUNCTIONS = [
-    "SUGGEST",
-    "ACCUSE",
-    "REFUTE",
-    "MOVE",
-    "UPDATE",
-    "DISPLAY",
-    "TURN"
-]
+# Message functions
+SUGGEST = 1
+ACCUSE = 2
+REFUTE = 3
+MOVE = 4
+UPDATE = 5
+DISPLAY = 6
+TURN = 7
 
 
 class MessageInterface:
@@ -18,13 +17,20 @@ class MessageInterface:
 
     @staticmethod
     def create_message(function, data):
-        """Create a new direct message and serialize its content."""
+        """Create a new message and serialize its content."""
         msg = Message()
         msg.function = function
         msg.data = data
 
         # Serializes the class into the form {"x": 1, "y": 2}
         return json.dumps(msg.__dict__)
+
+    @staticmethod
+    def process_message(serialdata):
+        """Deserialize data and prepare a python object."""
+        msg = Message()
+        msg = json.loads(serialdata)
+        return msg
 
 
 class Message:
@@ -34,3 +40,46 @@ class Message:
         self.function = None
         self.checksum = None
         self.data = None
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        """
+        SUGGEST:
+            {
+                "player":
+                "location":
+                "weapon":
+            }
+
+        ACCUSE:
+            {
+                "player":
+                "location":
+                "weapon":
+            }
+            
+        REFUTE:
+            {
+                TBD
+            }
+
+        MOVE:
+            {
+                TBD
+            }
+
+        UPDATE:
+            {
+                <serialized entire gamestate>
+            }
+
+        DISPLAY:
+            <data>
+
+        TURN:
+            <its your turn!>
+        
+        """
+        self.data = data
